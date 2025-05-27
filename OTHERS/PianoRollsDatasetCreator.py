a629224be7523de6ce8fb11b684064516370746e
MarioPaerle <paerle.music@gmail.com>
2025-05-27 20:04:27 +0200

    Correction to todo

--- OTHERS/PianoRollsDatasetCreator.py
+++ OTHERS/PianoRollsDatasetCreator.py
@@ -6,14 +6,17 @@ import warnings
 from tqdm import tqdm
 
 
-FOLDER = "MuseScoreMIDIS/"
+FOLDER = "MuseScoreMIDIS2/"
 files = [f for f in os.listdir(FOLDER) if f.endswith(".mid")]
 songs = []
 lenght = 256
-div = 4
+# div = 4
 for file in tqdm(files):
     for shift in range(0, 13):
-        midi = midi_to_segmented_pianorolls(FOLDER + file, k=lenght, shift=shift, div=div)
-        if isinstance(midi, np.ndarray):
-            songs.append(midi[:, 20:100, :])
+        for dd in range(1, 5):
+            midi = midi_to_segmented_pianorolls(FOLDER + file, k=lenght, shift=shift, div=dd)
+            if isinstance(midi, np.ndarray):
+                songs.append(midi[:, 20:100, :])
+                print(midi.shape)
 
+joblib.dump(songs, "MS2_big1_dataset.pkl", compress=9)
